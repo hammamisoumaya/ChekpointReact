e4c5644a554975e9efa40614b3cb7d8e62e952a4
hammamisoumaya <soumayahammami08@gmail.com>
2023-02-17 22:00:46 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 
 import './App.css';
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Alert from 'react-bootstrap/Alert';
 
 function App() {
   return (
     <div className="App">
      <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#"> <a href='/'> <img src='./asset/image/MCNC3.jpg' alt='logo' height={100}/>
+        <Navbar.Brand href="#"> <a href='/'> <img src='https://img.freepik.com/photos-gratuite/copie-espace-roses-fleurs_23-2148860032.jpg' height={100}/>
         </a> </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
@@ -24,7 +27,7 @@ function App() {
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
+            <Nav.Link href="#action2">About us</Nav.Link>
             <NavDropdown title="Link" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
@@ -51,56 +54,98 @@ function App() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-         
+        
     <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>Address 2</Form.Label>
+        <Form.Control placeholder="Apartment, studio, or floor" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Select defaultValue="Choose...">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" id="formGridCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
+
       <Button variant="primary" type="submit">
         Submit
       </Button>
-    </Form>    
+    </Form>
+  <br/>
+  <>
+      {[
+        'primary',
+        'secondary',
+        'success',
+        'danger',
+        'warning',
+        'info',
+        'light',
+        'dark',
+      ].map((variant) => (
+        <Alert key={variant} variant={variant}>
+          This is a {variant} alert with{' '}
+          <Alert.Link href="#">an example link</Alert.Link>. Give it a click if
+          you like.
+        </Alert>
+      ))}
+    </>
+
+
 
     <>
-      {[
-        'Primary',
-        'Secondary',
-        'Success',
-        'Danger',
-        'Warning',
-        'Info',
-        'Light',
-        'Dark',
-      ].map((variant) => (
-        <Card
-          bg={variant.toLowerCase()}
-          key={variant}
-          text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
-          style={{ width: '18rem' }}
-          className="mb-2"
-        >
-          <Card.Header>Header</Card.Header>
-          <Card.Body>
-            <Card.Title>{variant} Card Title </Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      ))}
+      <Card>
+      <Card.Text>
+      Organe des plantes supérieures, ou phanérogames, qui contient les organes reproducteurs entourés le plus souvent d'un périanthe, et dont les pièces peuvent être d'une couleur brillante ou d'un parfum agréable.
+          </Card.Text>
+        <Card.Img variant="top" src="https://i.pinimg.com/originals/c6/66/dc/c666dcfd2c52e0462866819cefc5d184.png" />
+        <Card.Body>
+        </Card.Body>
+      </Card>
+      <br />
+      <Card>
+        <Card.Body>
+          <Card.Text>
+          Nom donné par les anciens chimistes à certains solides plus ou moins brillants, obtenus par sublimation ou par une oxydation prolongée au contact de l'air (fleur d'arsenic, fleur de soufre, etc.).
+          </Card.Text>
+        </Card.Body>
+        <Card.Img variant="bottom" src="https://thumbs.dreamstime.com/b/fleurs-d-automne-22122594.jpg" />
+      </Card>
     </>
       
     </div>
